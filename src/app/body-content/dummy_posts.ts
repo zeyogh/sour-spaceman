9011778b8971073849522c4cd80fbe05f0275f42
Zeynep Oghan <zeynepo@a-t-g.com>
2025-04-04 08:24:36 -0500

    header takes up top/front of page

--- src/app/body-content/dummy_posts.ts
+++ src/app/body-content/dummy_posts.ts
@@ -15,4 +15,20 @@ export const DUMMY_POSTS = [
     signoff: '--Zeynep Again',
     date: '2025-06-21',
   },
+  {
+    id: '113',
+    title: 'Third Post',
+    textContent: 'Lorem Ipsum ffdffsfdsf...',
+    img: 'character_img.png',
+    signoff: '--Zeynep 3',
+    date: '2025-06-21',
+  },
+  {
+    id: '114',
+    title: 'Fourth Post',
+    textContent: 'Lorem Ipsum GRRRRRRRRR...',
+    img: 'character_img.png',
+    signoff: '--Zeynep Encore',
+    date: '2025-06-21',
+  },
 ];
