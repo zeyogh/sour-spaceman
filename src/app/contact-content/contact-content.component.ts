0d91323257103de753975a7b213fcbdb101f20d7
Zeynep Oghan <zeynepo@a-t-g.com>
2025-04-04 16:25:32 -0500

    setup for message submission form

--- src/app/contact-content/contact-content.component.ts
+++ src/app/contact-content/contact-content.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-contact-content',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-content.component.html',
-  styleUrl: './contact-content.component.scss'
+  styleUrl: './contact-content.component.scss',
 })
 export class ContactContentComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
 
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form');
+
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+
+      const subscription = this.form()
+        ?.valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'saved-message',
+              JSON.stringify({ email: value.email, message: value.message })
+            ),
+        });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+
+    const email = formData.form.value.email;
+    const message = formData.form.value.message;
+
+    formData.form.reset();
+  }
 }
