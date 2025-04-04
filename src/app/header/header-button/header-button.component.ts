7074d840a3c6e6e1bdc71329e2b9724731fea2f3
Zeynep Oghan <zeynepo@a-t-g.com>
2025-04-04 16:52:48 -0500

    goldButton as global class, all buttons right-clickable

--- src/app/header/header-button/header-button.component.ts
+++ src/app/header/header-button/header-button.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-button',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header-button.component.html',
   styleUrl: './header-button.component.scss',
 })
 export class HeaderButtonComponent {
   @Input({ required: true }) label!: string;
+  @Input({ required: true }) link!: string;
 }
