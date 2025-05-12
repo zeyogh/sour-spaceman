6ea5a2afe1cf8203b8c539b854880b2686d7ea48
Zeynep Oghan <zeynepo@a-t-g.com>
2025-05-12 15:23:45 -0500

    replace png images with webps to greatly reduce load time

--- src/app/games-content/dummy_games.ts
+++ src/app/games-content/dummy_games.ts
@@ -2,7 +2,7 @@ export const DUMMY_GAMES = [
   {
     id: '111',
     title: 'Biological Welfare',
-    logo: 'game_title.png',
+    logo: 'game_title.webp',
     description:
       'A vibrant 2D romp through the apocalypse, complete with a sentient compost bin. Features robot in a wig.',
     video: 'dQw4w9WgXcQ',
@@ -10,7 +10,7 @@ export const DUMMY_GAMES = [
   {
     id: '112',
     title: "Hitchhiking At Freddy's",
-    logo: 'game_title.png',
+    logo: 'game_title.webp',
     description: 'I need to get back to this.',
     video: 'dQw4w9WgXcQ',
   },
