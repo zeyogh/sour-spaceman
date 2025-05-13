6350124dbbf00007115f1938d8ecd71267f1a186
Zeynep Oghan <zeynepo@a-t-g.com>
2025-05-13 13:49:59 -0500

    use formbuilder for contact content

--- src/app/contact-content/contact-content.component.ts
+++ src/app/contact-content/contact-content.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactForm } from './contact-content.model';
 
 @Component({
   selector: 'app-contact-content',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact-content.component.html',
   styleUrl: './contact-content.component.scss',
 })
 export class ContactContentComponent {
-  form: ContactForm = {
-    name: '',
-    email: '',
-    message: '',
-  };
+  formBuilder = inject(FormBuilder);
 
-  send() {
-    // emailjs.send();
+  form = this.formBuilder.group({
+    name: [''],
+    email: [''],
+    message: [''],
+  });
+
+  submitForm() {
+    console.log(this.form.value);
   }
 }
