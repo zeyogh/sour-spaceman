f6876bb629367d0f20bb73384e255afc024f7cfb
Zeynep Oghan <zeynepo@a-t-g.com>
2025-06-19 11:43:25 -0500

    sending params

--- src/app/blog-content/blog-entry-page/blog-entry-page.component.ts
+++ src/app/blog-content/blog-entry-page/blog-entry-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-blog-entry-page',
   imports: [],
   templateUrl: './blog-entry-page.component.html',
-  styleUrl: './blog-entry-page.component.scss'
+  styleUrl: './blog-entry-page.component.scss',
 })
 export class BlogEntryPageComponent {
+  #route = inject(ActivatedRoute);
 
+  title = '';
+  img = '';
+
+  constructor() {
+    const snapShot = this.#route.snapshot.queryParamMap;
+    this.title = snapShot.get('title') ?? '';
+    this.img = snapShot.get('img') ?? '';
+  }
 }
